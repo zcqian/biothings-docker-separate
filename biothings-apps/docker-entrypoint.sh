23f0e90e2241bdda14d295070b172a3388e07e98
Zhongchao Qian <zqian@scripps.edu>
2021-03-18 17:23:38 -0700

    traps fine, but goes into an inf. loop when python is killed

--- biothings-apps/docker-entrypoint.sh
+++ biothings-apps/docker-entrypoint.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
 
+function bthub_pids()
+{
+    ps -ef | grep 'python' | grep 'hub.py' | awk '{if ($2 != "1") print $2}'
+}
+
+
 function stop()
 {
     echo "Stopping ..."
-    kill -INT $biothings_pid
+    for biothings_pid in $(bthub_pids)
+    do 
+        kill -INT $biothings_pid
+    done
 }
 
+
+
 trap stop SIGTERM SIGINT
+
+eval $@
+
+
+biothings_pids=$(bthub_pids)
+while [ "$biothings_pids" ]
+do
+    echo "waiting on $biothings_pids ... "
+    wait $biothings_pids  # all exited
+    # find new pids (if any)
+    biothings_pids=$(bthub_pids)
+done
+
+wait
